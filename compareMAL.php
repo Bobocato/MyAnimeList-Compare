de961f24080fa8b7fc32c4965450f83de33bd022
Bobocato <jessearff@googlemail.com>
2017-09-23 12:45:11 +0200

    Added multi User support

--- compareMAL.php
+++ compareMAL.php
@@ -5,50 +5,182 @@
  * Date: 21-Sep-17
  * Time: 11:38
  */
-$userNames = json_decode($_POST["names"]);
-//echo nl2br("UserNames: \n");
-//var_dump($userNames);
-$lists = array();
-//echo nl2br("Anzahl User: \n");
-//var_dump(count($userNames));
-for ($i = 0; $i < count($userNames); $i++) {
-    $xml = file_get_contents('https://myanimelist.net/malappinfo.php?u=' . $userNames[$i] . '&status=all&type=anime');
-    $xmlObj = simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xmlObj);
-    $array = json_decode($json, true);
-    $lists[$i] = $array;
-}
-//echo $lists;
-//echo nl2br("Item: \n");
-//var_dump($lists[0]["anime"][0]["series_title"]);
-$duplicates = array();
-$checkList = 1;
-//for($j = 0; $j < count($lists);$j++){
-for ($k = 0; $k < count($lists[0]["anime"]); $k++) {
-    if ($lists[0]["anime"][$k]["series_title"] != NULL && $lists[0]["anime"][$k]["my_status"] == 6) {
-        $currEntry = $lists[0]["anime"][$k];
-        for ($t = 0; $t < count($lists[1]["anime"]); $t++) {
-/*          echo $currEntry["series_title"];
-            echo " t = " . $t . " Checklist = ". $checkList. " ";
-            var_dump($lists[$checkList]["anime"][$t]["series_title"]);*/
 
-            if ($currEntry["series_title"] == $lists[$checkList]["anime"][$t]["series_title"]) {
-                $duplicates[count($duplicates)] = $currEntry;
-            }
-            if($checkList > count($lists)){
-                $checkList = 1;
+function getList($userNames) {
+    for ($i = 0; $i < count($userNames); $i++) {
+        $xml = file_get_contents('https://myanimelist.net/malappinfo.php?u=' . $userNames[$i + 1] . '&status=all&type=anime');
+        $xmlObj = simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA);
+        $json = json_encode($xmlObj);
+        $array = json_decode($json, true);
+        $lists[$i] = $array;
+    }
+    return $lists;
+}
+
+function compareLists($lists, $listCounter, $checkCounter) {
+    $duplicates = array();
+    for ($k = 0; $k < count($lists[$listCounter]["anime"]); $k++) {
+        if ($lists[$listCounter]["anime"][$k]["series_title"] != NULL && $lists[$listCounter]["anime"][$k]["my_status"] == 6) {
+            $currEntry = $lists[$listCounter]["anime"][$k];
+            for ($t = 0; $t < count($lists[$checkCounter]["anime"]); $t++) {
+                if ($currEntry["series_title"] == $lists[$checkCounter]["anime"][$t]["series_title"] && $lists[$checkCounter]["anime"][$t]["my_status"] == 6) {
+                    $duplicates[count($duplicates)] = $currEntry;
+                }
             }
         }
     }
+    return $duplicates;
 }
-//echo nl2br("Duplicates: \n");
-//var_dump($duplicates);
-//}
 
-echo json_encode($duplicates);
+function compareDuplicates($duplicates1, $duplicates2) {
+    $compared = array();
+    for ($i = 0; $i < count($duplicates1); $i++) {
+        $currEntry = $duplicates1[$i];
+        for ($j = 0; $j < count($duplicates2); $j++) {
+            if ($currEntry["series_title"] == $duplicates2[$j]["series_title"]) {
+                $compared[count($compared)] = $currEntry;
+            }
+        }
+    }
+    return $compared;
+}
 
-//echo nl2br("Lists: \n");
-//var_dump($lists);
+function headerPage() {
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Compare your MAL</title>
+        <!-- jQuery and Bootstrap -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-// var_dump(file_get_contents('https://myanimelist.net/malappinfo.php?u=bobocato&status=all&type=anime'));
-// $url = 'https://myanimelist.net/malappinfo.php?u=' . $userNames . '&status=all&type=anime';
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- meta for mobile devices -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- My js and css -->
+        <script src="compareMal.js"></script>
+        <link rel="stylesheet" href="compareMal.css">
+    </head>
+    <?php
+}
+
+function errorPage() {
+    ?>
+    <body>
+    <div class="page-header center">
+        <a href="index.html"><h1>MyAnimeList Compare</h1></a>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h2>Something went wrong! FeelsBadMan</h2>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+    </div>
+    </body>
+    </html>
+    <?php
+}
+
+function resultPage($results) {
+    ?>
+    <body>
+    <div class="page-header center">
+        <a href="index.html"><h1>MyAnimeList Compare</h1></a>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <div id="result">
+                    <table class="table table-hover" id="resultTable">
+                        <thead>
+                        <tr>
+                            <th>Nr.</th>
+                            <th>Picture</th>
+                            <th>Name</th>
+                            <th>Episodes</th>
+                        </tr>
+                        </thead>
+                        <tbody id="tableBody"></tbody>
+                        <?php
+                        for ($i = 0; $i < count($results); $i++) {
+                            ?>
+                            <tr class="clickable" id="<?php echo $results[$i]["series_animedb_id"] ?>">
+                                <td id="<?php echo $results[$i]["series_animedb_id"] ?>">
+                                    <div id="<?php echo $results[$i]["series_animedb_id"] ?>"> <?php echo($i + 1) ?> </div>
+                                </td>
+                                <td id="<?php echo $results[$i]["series_animedb_id"] ?>">
+                                    <img id="<?php echo $results[$i]["series_animedb_id"] ?>"
+                                         src="<?php echo $results[$i]["series_image"] ?>"/>
+                                </td>
+                                <td id="<?php echo $results[$i]["series_animedb_id"] ?>">
+                                    <div id="<?php echo $results[$i]["series_animedb_id"] ?>"> <?php echo $results[$i]["series_title"] ?></div>
+                                </td>
+                                <td id="<?php echo $results[$i]["series_animedb_id"] ?>">
+                                    <div id="<?php echo $results[$i]["series_animedb_id"] ?>"> <?php echo $results[$i]["series_episodes"] ?> </div>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                    </table>
+                </div>
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+    </div>
+    </body>
+    </html>
+    <?php
+}
+
+$userNames = $_GET;
+
+switch (count($userNames)) {
+    case 0:
+        headerPage();
+        errorPage();
+        break;
+    case 1:
+        headerPage();
+        errorPage();
+        break;
+    case 2:
+        $lists = array();
+        $lists = getList($userNames);
+        $duplicates = compareLists($lists, 0, 1);
+        headerPage();
+        resultPage($duplicates);
+        //var_dump($duplicates);
+        break;
+    case 3:
+        $lists = array();
+        $lists = getList($userNames);
+        $duplicates1 = compareLists($lists, 0, 1);
+        $duplicates2 = compareLists($lists, 0, 2);
+        $compared = compareDuplicates($duplicates1, $duplicates2);
+        headerPage();
+        resultPage($compared);
+        //var_dump($compared);
+        break;
+    case 4:
+        $lists = array();
+        $lists = getList($userNames);
+        $duplicates1 = compareLists($lists, 0, 1);
+        $duplicates2 = compareLists($lists, 2, 3);
+        $compared = compareDuplicates($duplicates1, $duplicates2);
+        headerPage();
+        resultPage($compared);
+        //var_dump($compared);
+        break;
+}
--- compareMal.js
+++ compareMal.js
@@ -2,77 +2,27 @@
  * Created by Jesse on 20-Sep-17.
  */
 var $ = jQuery;
-console.log("LuuL");
-function showShows(showList) {
-    fragment = document.createDocumentFragment();
-    for (var i = 0; i < showList.length; i++){
-        $('<tr class="clickable" id="' + showList[i]["series_animedb_id"] + '"></trclass>').append(
-            $('<td id="' + showList[i]["series_animedb_id"] + '"></td>').append(
-                $('<div id="' + showList[i]["series_animedb_id"] + '">'+ (i + 1) +'.</div>')
-            ),
-            $('<td id="' + showList[i]["series_animedb_id"] + '"></td>').append(
-                $('<image id="' + showList[i]["series_animedb_id"] + '" src="'+ showList[i]["series_image"] +'"/>')
-            ),
-            $('<td id="' + showList[i]["series_animedb_id"] +'"></td>').append(
-                $('<div id="' + showList[i]["series_animedb_id"] + '">'+ showList[i]["series_title"] +'</div>')
-            ),
-            $('<td id="' + showList[i]["series_animedb_id"] + '"></td>').append(
-                $('<div id="' + showList[i]["series_animedb_id"] + '">'+ showList[i]["series_episodes"] +'</div>')
-            )
-        ).appendTo(fragment);
+
+$(document).ready(function () {
+    //Eventlistner to add extra users
+    if (document.getElementById("addUser") !== null) {
+        document.getElementById("addUser").addEventListener("click", function () {
+            if (document.getElementsByClassName("userin").length < 4) {
+                if (document.getElementsByClassName("userin").length <= 4) {
+                    if (document.getElementsByClassName("userin").length % 2 === 0) {
+                        document.getElementById("leftUserfield").innerHTML += '<label for="user3">Third User:</label><input type="text" name="3" class="form-control userin" class="userin" id="user3">'
+                    } else {
+                        document.getElementById("rightUserfield").innerHTML += '<label for="user4">Fourth User:</label><input type="text" name="4" class="form-control userin" class="userin" id="user4">'
+                        document.getElementById("addUser").disabled = true;
+                    }
+                }
+            }
+        });
     }
-    $("#tableBody").append(fragment);
-
-
     $(".clickable").click(function (event) {
         var url = "https://myanimelist.net/anime/" + event.target.id;
         console.log(event.target.id);
         console.log(url);
         window.open(url, "_blank");
     });
-
-}
-
-function changeView() {
-    $("#form").remove();
-    document.getElementById("result").style.display = "block";
-}
-
-$(document).ready(function () {
-    //Eventlistner to add extra users
-    document.getElementById("addUser").addEventListener("click", function () {
-        if (document.getElementsByClassName("userin").length < 4) {
-            if (document.getElementsByClassName("userin").length <= 4) {
-                if (document.getElementsByClassName("userin").length % 2 === 0) {
-                    document.getElementById("leftUserfield").innerHTML += '<label for="user3">Third User:</label><input type="text" class="form-control userin" class="userin" id="user3">'
-                } else {
-                    document.getElementById("rightUserfield").innerHTML += '<label for="user4">Fourth User:</label><input type="text" class="form-control userin" class="userin" id="user4">'
-                    document.getElementById("addUser").disabled = true;
-                }
-            }
-        }
-    });
-    //Eventlistner to start the comparison
-    document.getElementById("compare").addEventListener("click", function () {
-        var userName = [];
-        for (var i = 0; i < document.getElementsByClassName("userin").length; i++) {
-            userName[i] = document.getElementsByClassName("userin")[i].value;
-        }
-        var json = JSON.stringify(userName);
-        console.log(json);
-
-        $.ajax({
-            type: "POST",
-            url: "compareMAL.php",
-            dataType: "json",
-            data: {"names": json},
-            success: function (response) {
-                console.log(response);
-                changeView();
-                showShows(response);
-            }
-        })
-        $("#compare").disabled = true;
-    })
-
 });
